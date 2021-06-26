b0a14a2c9b26fee265cda771d1d1a48c564f8bc7
Andres Arcila <andresarcila2@me.com>
2021-06-26 00:58:12 -0500

    conecto db mysql y res movimiento

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MovimientoModule } from './movimiento/movimiento.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    MovimientoModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'AArcila',
+      password: '',
+      database: 'nestjs',
+      entities: [],
+      synchronize: true,
+      socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
